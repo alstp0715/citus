913af405af8190fab122a38d2f27c2fc3d8c6001
alstp0715 <alstp0715@gmail.com>
2022-05-13 14:33:54 +0900

    ems

---
+++ EMS-main/js/EMS.js
@@ -0,0 +1,87 @@
+//엑셀데이터 업로드
+function upload(){
+    const input = event.target;
+    const reader = new FileReader();
+    reader.onload = function(){ 
+      const fdata = reader.result;
+      const read_buffer = XLSX.read(fdata, {type : 'binary'});
+    read_buffer.SheetNames.forEach(function(sheetName){ 
+      const rowdata =XLSX.utils.sheet_to_json(read_buffer.Sheets[sheetName]);
+      const dataUpload = JSON.stringify(rowdata);
+      const dis_upload = document.getElementById("file_upload");
+      dis_upload.innerHTML = dataUpload.replace(/\"|:|\[{|\}]/g, ' ');
+     }
+    )
+  };
+    reader.readAsBinaryString(input.files[0]); 
+  } 
+
+  //로그인
+  function onLogin(){
+    const ID = document.getElementById("ID");
+    const PW = document.getElementById("PW");
+    if(ID.value=="asd" && PW.value=="asd"){
+      alert("로그인 성공");
+      location.href ="/practice/practice_total.html";
+    }else{
+      alert("로그인 실패");
+    }
+    ID.value = "";
+    PW.value = "";
+  } 
+
+
+  const tablePageBtn = document.querySelector("#table_btn");
+  const homePageBtn = document.querySelector("#home_btn");
+  const HIDDEN_classname = 'hidden'; 
+    function changePage(form){
+            const hasTableclass = tablePageBtn.classList.contains(HIDDEN_classname);
+            fetch(form).then(function(response){
+            response.text().then(function(text){
+              if(!hasTableclass){
+                tablePageBtn.classList.add(HIDDEN_classname);
+                homePageBtn.classList.remove(HIDDEN_classname);
+                document.querySelector('#tableForm').innerHTML = text;
+                  }else {
+                    homePageBtn.classList.add(HIDDEN_classname);
+                    tablePageBtn.classList.remove(HIDDEN_classname);
+                    document.querySelector('#homeForm').innerHTML = text;
+                  }return
+                });
+              });
+  }
+  // function homePage(name){
+  //   fetch(name).then(function(response){
+  //         response.text().then(function(text){
+  //           tablePageBtn.classList.remove(HIDDEN_classname);
+  //           homePageBtn.classList.add(HIDDEN_classname);
+  //         document.querySelector('#home_form').innerHTML = text;
+  //                 })
+  //             });
+  // }
+const clock = document.querySelector("#clock");
+const date_dis = document.querySelector('#date')
+
+function sayHello(){
+  const date = new Date();
+  const year = String(date.getFullYear())
+  const month = String(date.getMonth())
+  const Ddate = String(date.getDate())
+  // const day = String(date.getDay())
+  const hours =String(date.getHours()).padStart(2,"0");
+  const minutes =String(date.getMinutes()).padStart(2,"0");
+  const seconds = String(date.getSeconds()).padStart(2,"0");
+  let weekday = new Array(7);
+  weekday[0] = "일요일";
+  weekday[1] = "월요일";
+  weekday[2] = "화요일";
+  weekday[3] = "수요일";
+  weekday[4] = "목요일";
+  weekday[5] = "금요일";
+  weekday[6] = "토요일";
+  const d = weekday[date.getDay()];
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
+  date_dis.innerText =` ${year}-${month}-${Ddate} ${d}`
+}
+sayHello()
+setInterval(sayHello,1000);
---
+++ EMS-main/js/chart.js
@@ -0,0 +1,143 @@
+new Chart(document.querySelectorAll('.line_chart'), {
+    type: 'line',
+    data: {
+      labels: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23],
+      datasets: [{ 
+          data: [0,110,200,350,400,300,500,600,500,800,400,100],
+          label: "발전량",
+          borderColor: "#3e95cd",
+          fill: true
+        },{ 
+            label: "충전량",
+            borderColor: "#8e5ea2",
+            fill: false
+          },{ 
+            label: "방전량",
+            borderColor: "tomato",
+            fill: false
+          },
+      ]
+    }
+  })
+
+  var gaugeOptions = {
+    chart: {
+        type: 'solidgauge'
+    },
+
+    title: null,
+
+    pane: {
+        center: ['50%', '65%'],
+        size: '130%',
+        startAngle: -90,
+        endAngle: 90,
+        background: {
+            backgroundColor:
+                Highcharts.defaultOptions.legend.backgroundColor || '#EEE',
+            innerRadius: '60%',
+            outerRadius: '100%',
+            shape: 'arc'
+        }
+    },
+
+    exporting: {
+        enabled: false
+    },
+
+    tooltip: {
+        enabled: false
+    },
+
+    // the value axis
+    yAxis: {
+        stops: [
+            [0.1, '#55BF3B'], // green
+            [0.5, '#DDDF0D'], // yellow
+            [0.9, '#DF5353'] // red
+        ],
+        lineWidth: 0,
+        tickWidth: 0,
+        minorTickInterval: null,
+        tickAmount: 2,
+        title: {
+            y: -70
+        },
+        labels: {
+            y: 16
+        }
+    },
+
+    plotOptions: {
+        solidgauge: {
+            dataLabels: {
+                y: 5,
+                borderWidth: 0,
+                useHTML: true
+            }
+        }
+    }
+};
+
+// The speed gauge
+var chartSpeed = Highcharts.chart('container-speed', Highcharts.merge(gaugeOptions, {
+    yAxis: {
+        min: 0,
+        max: 5000,
+        title: {
+            text: 'Speed'
+        }
+    },
+
+    credits: {
+        enabled: false
+    },
+
+    series: [{
+        name: 'Speed',
+        data: [4000],
+        dataLabels: {
+            format:
+                '<div style="text-align:center">' +
+                '<span style="font-size:25px">{y}</span><br/>' +
+                '<span style="font-size:12px;opacity:0.4">kW</span>' +
+                '</div>'
+        },
+        tooltip: {
+            valueSuffix: ' kW'
+        }
+    }]
+
+}));
+
+// The RPM gauge
+var chartRpm = Highcharts.chart('container-rpm', Highcharts.merge(gaugeOptions, {
+    yAxis: {
+        min: 0,
+        max: 5000,
+        title: {
+            text: 'RPM'
+        }
+    },
+    
+    credits: {
+      enabled: false
+  },
+
+    series: [{
+        name: 'RPM',
+        data: [1000],
+        dataLabels: {
+            format:
+                '<div style="text-align:center">' +
+                '<span style="font-size:25px">{y:.1f}</span><br/>' +
+                '<span style="font-size:12px;opacity:0.4">' +
+                'kW' +
+                '</span>' +
+                '</div>'
+        },
+        tooltip: {
+            valueSuffix: ' KW'
+        }
+    }]
+}));
